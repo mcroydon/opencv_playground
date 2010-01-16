ccfb51b0a9b0cb823c08135db0c490fd905ad98f
Matt Croydon <matt@lawrence.com>
2010-01-15 22:08:57 -0600

    Added Canny edge detection example.

---
+++ canny.py
@@ -0,0 +1,17 @@
+import cv
+
+# Load an image as grayscale
+image = cv.LoadImage('image.jpg', 0)
+
+# Get the size of the image
+size = cv.GetSize(image)
+
+# Create an image to save edge data to
+edges_image = cv.CreateImage(size, 8, 1)
+
+# Canny edge detection
+# http://opencv.willowgarage.com/documentation/python/feature_detection.html
+cv.Canny(image, edges_image, 5, 10)
+
+# Save the edges image
+cv.SaveImage('edges_image.jpg', edges_image)
